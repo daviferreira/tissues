0b125d4808df1375f9c69ec5fc4d9c2687e2b52b
Davi Ferreira <davi.ferreira@corp.globo.com>
2012-02-08 09:45:13 -0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tissues::Application.config.secret_token = 'caa5f9b91c9f64301849f5d8e8eb71da5141b49c910606b8ebd8608891896a5b7235f0f1ab7df54e8218070a0a5aeb5de760847a944edfd4823a51e60a21919f'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Tissues::Application.routes.draw do
+  root :to => "pages#index"
+end
