e08fc1b21934e1821d338b1d43752644d516ea14
Davi Ferreira <davi.ferreira@corp.globo.com>
2012-03-02 13:33:03 -0300

    Better sign in and forgot your password forms.

--- spec/requests/session_pages_spec.rb
+++ spec/requests/session_pages_spec.rb
@@ -11,4 +11,18 @@ describe "Session pages" do
     it { should have_selector('ul.breadcrumb > li.active > a', :text => "Create an account", :href => new_user_registration_path) }
   end
   
+  describe "sign in" do
+    before { visit new_user_session_path }
+    it { should have_selector('title', text: 'Sign in | Tissues') }
+    it { should have_selector('ul.breadcrumb > li > a', :text => "Home", :href => root_path) }
+    it { should have_selector('ul.breadcrumb > li.active > a', :text => "Sign in", :href => new_user_session_path) }
+  end
+  
+  describe "forgot your password" do
+    before { visit new_user_password_path }
+    it { should have_selector('title', text: 'Forgot your password? | Tissues') }
+    it { should have_selector('ul.breadcrumb > li > a', :text => "Home", :href => root_path) }
+    it { should have_selector('ul.breadcrumb > li.active > a', :text => "Forgot your password?", :href => new_user_password_path) }
+  end
+  
 end
